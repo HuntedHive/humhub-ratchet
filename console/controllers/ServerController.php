fef14b76f886b6a7862d47e58183c0636acd4652
Den Volkov <volkovdenya@gmail.com>
2016-07-20 15:45:45 +0300

    fixed server start whit params

--- console/controllers/ServerController.php
+++ console/controllers/ServerController.php
@@ -14,6 +14,9 @@ class ServerController extends \yii\console\Controller
 
     public function actionStart()
     {
+        $address = !empty(Yii::$app->params['address'])?Yii::$app->params['address']:'0.0.0.0';
+        $port = !empty(Yii::$app->params['port'])?Yii::$app->params['port']:8080;
+
         $server = IoServer::factory(
             new HttpServer(
                 new WsServer(
